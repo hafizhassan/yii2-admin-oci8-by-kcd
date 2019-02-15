e28327b16fee225ed84ca9d0b95f0c64bdf6a43c
hafizhassan <hafiz.hassan@hotmail.my>
2019-02-15 10:22:21 +0800

    Update view.php

--- views/assignment/view.php
+++ views/assignment/view.php
@@ -12,7 +12,7 @@ $role_kcd_admin=ImoHelper::getRole("Kulliyyah Administrator");
 if($role_cps_admin){
     $avaliable=$avaliable;
 }else{
-    $remove=array('Kulliyyah Administrator','CPS Administrator');
+    $remove=array('CPS Administrator');
     $new_array=array_diff($avaliable['Roles'],$remove);
     $avaliable['Roles']=$new_array;
     $avaliable['Permissions']=array( );
